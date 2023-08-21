e511973b533e36a63f37320057c086620be47ef0
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-21 19:45:47 +0300

    JSX frame is added to TaskHookForm

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from "react";
 
 export default function TaskHookForm() {
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form className="taskForm" onSubmit={handleSubmit}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input className="input-text" id="title" type="text" />
+        <p className="input-error">{formErrors.title}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+        ></textarea>
+        <p className="input-error">{formErrors.description}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input type="checkbox" checked={formData.people.includes(p)} />
+              {p}
+            </label>
+          ))}
+        </div>
+        <p className="input-error">{formErrors.people}</p>
+      </div>
+
+      <div className="form-line">
+        <button
+          className="submit-button"
+          type="submit"
+          disabled={buttonDisabled}
+        >
+          Kaydet
+        </button>
+      </div>
+    </form>
+  );
 }
