ef503e3f423f12dd95322ec6d31a376108726bf5
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-21 20:33:14 +0300

    Error messages are added

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { nanoid } from "nanoid";
 
-export default function TaskHookForm() {
-  const { register, handleSubmit } = useForm();
+export default function TaskHookForm({ kisiler, submitFn }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      people: [],
+    },
+    mode: "onBlur",
+  });
+
+  function onSubmit(e) {
+    e.preventDefault();
+    submitFn({
+      /* ...formData,
+      id: nanoid(5),
+      status: "yapılacak", */
+    });
+  }
 
   return (
-    <form className="taskForm" onSubmit={handleSubmit}>
+    <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
       <div className="form-line">
         <label className="input-label" htmlFor="title">
           Başlık
         </label>
-        <input className="input-text" id="title" type="text" />
-        <p className="input-error">{formErrors.title}</p>
+        <input
+          className="input-text"
+          id="title"
+          type="text"
+          {...register("title", {
+            required: "Task başlığı yazmalısınız",
+            minLength: {
+              value: 3,
+              message: "Task başlığı en az 3 karakter olmalı",
+            },
+          })}
+        />
+        <p className="input-error">{errors.title?.message}</p>
       </div>
 
       <div className="form-line">
@@ -22,8 +54,15 @@ export default function TaskHookForm() {
           className="input-textarea"
           rows="3"
           id="description"
+          {...register("description", {
+            required: "Task açıklaması yazmalısınız",
+            minLength: {
+              value: 10,
+              message: "Task açıklaması en az 10 karakter olmalı",
+            },
+          })}
         ></textarea>
-        <p className="input-error">{formErrors.description}</p>
+        <p className="input-error">{errors.description?.message}</p>
       </div>
 
       <div className="form-line">
@@ -31,20 +70,28 @@ export default function TaskHookForm() {
         <div>
           {kisiler.map((p) => (
             <label className="input-checkbox" key={p}>
-              <input type="checkbox" checked={formData.people.includes(p)} />
+              <input
+                type="checkbox"
+                {...register("people", {
+                  minLength: {
+                    value: 1,
+                    message: "Lütfen en az bir kişi seçin",
+                  },
+                  maxLength: {
+                    value: 3,
+                    message: "En fazla 3 kişi seçebilirsiniz",
+                  },
+                })}
+              />
               {p}
             </label>
           ))}
         </div>
-        <p className="input-error">{formErrors.people}</p>
+        <p className="input-error">{errors.people?.message}</p>
       </div>
 
       <div className="form-line">
-        <button
-          className="submit-button"
-          type="submit"
-          disabled={buttonDisabled}
-        >
+        <button className="submit-button" type="submit" disabled={!isValid}>
           Kaydet
         </button>
       </div>
